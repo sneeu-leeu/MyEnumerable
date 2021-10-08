1b2972bc4ba5418687e9035095f7685e7d633f66
sheylaPozo <shiggipoke@gmail.com>
2021-10-08 03:19:12 -0500

    add my class rb and test cases

---
+++ MyClass.rb
@@ -0,0 +1,17 @@
+def each
+    def each
+        i = 0
+        while i < @list.length
+        yield @list[i] if block_given?
+        i += 1
+    end
+end
+
+# test cases 
+
+list = MyList.new(1, 2, 3, 4)
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter(&:even?)
