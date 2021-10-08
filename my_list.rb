e29496808a28c616bb0fbbc49e8e7faa6b965b37
sneeu-leeu <sa070197@gmail.com>
2021-10-08 15:56:11 +0200

    Update and fix my_list

--- my_list.rb
+++ my_list.rb
@@ -8,4 +8,20 @@ class MyList
   def initialise(*arg)
     @list = arg
   end
+
+  def each
+    i = 0
+    while i < @list.length
+    yield @list[i] if block_given?
+    i += 1
+  end
 end
+
+# test cases 
+
+list = MyList.new(1, 2, 3, 4)
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter(&:even?)
